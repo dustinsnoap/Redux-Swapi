c26344693f0f74974a0a1539d72c4b23f73f71b9
Dustin Snoap <45376430+dustinsnoap@users.noreply.github.com>
2019-04-15 22:50:21 -0400

    Revert "MVP"

--- src/index.js
+++ src/index.js
@@ -1,25 +1,24 @@
-//react
-import React from "react"
-import ReactDOM from "react-dom"
-//redux
-import {applyMiddleware, createStore} from "redux"
-import {Provider} from "react-redux"
-//middleware
-import logger from 'redux-logger'
-import thunk from 'redux-thunk'
-//local
-import App from "./App"
-import rootReducer from "./reducers"
-import "./index.css"
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import rootReducer from "./reducers";
+// needed dependancies
+// applyMiddleware from redux
+// thunk from redux-thunk
+// logger from redux-logger
+// rootReducer from ./reducers
 
 const store = createStore(
-  rootReducer,
-  applyMiddleware(thunk, logger)
-)
+  rootReducer
+  /* applyMiddleware goes here */
+);
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById("root")
-)
+);
--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,32 +1,14 @@
-import {FETCH, SUCCESSFUL_HUNT, RETURNED_WITH_NOTHING} from "../actions"
-
+import /* we need our action types here*/ "../actions";
 const initialState = {
-  characters: [],
-  fetching: false,
-  error: null,
-}
-
+  characters: []
+  // Array characters, Boolean fetching, null error.
+};
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
-    case FETCH:
-      return {
-        ...state,
-        fetching: true,
-        error: null,
-      }
-    case SUCCESSFUL_HUNT:
-      return {
-        ...state,
-        fetching: false,
-        characters: [...state.characters, ...action.payload]
-      }
-    case RETURNED_WITH_NOTHING:
-      return {
-        ...state,
-        fetching: false,
-        error: action.payload,
-      }
+    // Fill me in with the important reducers
+    // action types should be FETCHING, SUCCESS and FAILURE
+    // your switch statement should handle all of these cases.
     default:
       return state;
   }
-}
+};
